19aedabc2c9071f7b5a20ee47e84228013b0ab49
sebjak <sjk90@gmail.com>
2015-01-06 20:55:15 +0000

    add changes lab3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var lab1_1 = require("./lab/lab1_1").lab
+var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./example_1").lab;
-var lab2 = require("./lab/lab2").lab
-var lab2_2 = require("./lab/lab2_2").lab
-var lab3 = require("./lab/lab3_1").lab
+var lab2 = require("./lab/lab2").lab;
+var lab2_2 = require("./lab/lab2_2").lab;
+var lab3 = require("./lab/lab3_1").lab;
 var PORT = 8080;
 
 
@@ -11,7 +11,8 @@ var urlMap = [
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
 	{path: "/lab2", action: lab2},
-	{path: "/runInstance", action: lab2_2}
+	{path: "/runInstance", action: lab2_2},
+	{path: "/lab3", action: lab3},
 ];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab3_1.js
+++ lab/lab3_1.js
@@ -1,14 +1,14 @@
 
 var AWS = require('aws-sdk');
-var autoscaling = new AWS.AutoScaling();
+//var autoscaling = new AWS.AutoScaling();
 
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
 console.log("Lab 3");
-	autoscaling.setDesiredCapacity(params, function(err, data) {
-	  if (err) console.log(err, err.stack); // an error occurred
-	  else     console.log(data);           // successful response
+	 new AWS.AutoScaling().setDesiredCapacity(params, function(err, data) {
+	  if (err) callback(err, null); // an error occurred
+	  else     callback(null,data);           // successful response
 	});
 	
 }
